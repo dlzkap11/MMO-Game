1d5a4308ec62c8dfefcec72088ad852523068e39
dlzkap11 <dlzkap11@gmail.com>
2025-02-07 04:35:12 +0900

    TileMap
    --타일 팔레트, 레이어, 충돌지역 설정

---
+++ Client/Assets/Editor/MapEditor.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+using System.IO;
+
+
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+public class MapEditor
+{
+#if UNITY_EDITOR
+
+    // 단축키 % (Ctrl), #(Shift), & (Alt)
+    [MenuItem("Tools/GenerateMap")]
+    private static void GenerateMap()
+    {
+        GameObject[] gameObjects = Resources.LoadAll<GameObject>("Prefabs/Map");
+
+        foreach (GameObject go in gameObjects)
+        {
+            Tilemap tm = Util.FindChild<Tilemap>(go, "Tilemap_Collision", true);
+
+            using (var writer = File.CreateText($"Assets/Resources/Map/{go.name}.txt"))
+            {
+                // 맵의 크기
+                writer.WriteLine(tm.cellBounds.xMin);
+                writer.WriteLine(tm.cellBounds.xMax);
+                writer.WriteLine(tm.cellBounds.yMin);
+                writer.WriteLine(tm.cellBounds.yMax);
+
+
+                // 타일을 찾아서 표시 있으면 1 없으면 0
+                for (int y = tm.cellBounds.yMax; y >= tm.cellBounds.yMin; y--)
+                {
+                    for (int x = tm.cellBounds.xMin; x <= tm.cellBounds.xMax; x++)
+                    {
+                        TileBase tile = tm.GetTile(new Vector3Int(x, y, 0));
+                        if (tile != null)
+                            writer.Write("1");
+                        else
+                            writer.Write("0");
+                    }
+                    writer.WriteLine();
+
+                }
+            }
+        }
+
+        
+
+    }
+
+
+
+#endif
+}
