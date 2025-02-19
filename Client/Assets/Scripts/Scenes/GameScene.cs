89e0080480a7cf3d0fddf0e022813c09fd6d6067
dlzkap11 <dlzkap11@gmail.com>
2025-02-19 17:54:51 +0900

    MultiSetting
    -protobuf 이식 및 빌드 테스트

--- Client/Assets/Scripts/Scenes/GameScene.cs
+++ Client/Assets/Scripts/Scenes/GameScene.cs
@@ -12,28 +12,34 @@ public class GameScene : BaseScene
 
         Managers.Map.LoadMap(1);
 
-        GameObject player = Managers.Resource.Instantiate("Creature/Player");
-        player.name = "Player";
-        Managers.Object.Add(player);
-
-        for (int i = 0; i < 5; i++)
-        {
-            GameObject monster = Managers.Resource.Instantiate("Creature/Monster");
-            monster.name = $"Monster_{i + 1}";
-
-            // 랜덤 위치 스폰
-            Vector3Int pos = new Vector3Int()
-            {
-                x = Random.Range(-16, 14),
-                y = Random.Range(-7, 6)
-            };
+        Screen.SetResolution(640, 480, false);
 
 
-            MonsterController mc = monster.GetComponent<MonsterController>();
-            mc.CellPos = pos;
 
-            Managers.Object.Add(monster);
-        }
+        //GameObject player = Managers.Resource.Instantiate("Creature/Player");
+        //player.name = "Player";
+        //Managers.Object.Add(player);
+
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    GameObject monster = Managers.Resource.Instantiate("Creature/Monster");
+        //    monster.name = $"Monster_{i + 1}";
+
+        //    // 랜덤 위치 스폰
+        //    Vector3Int pos = new Vector3Int()
+        //    {
+        //        x = Random.Range(-16, 14),
+        //        y = Random.Range(-7, 6)
+        //    };
+
+        //    MonsterController mc = monster.GetComponent<MonsterController>();
+        //    mc.CellPos = pos;
+
+        //    Managers.Object.Add(monster);
+        //}
+
+
+
 
         //Managers.UI.ShowSceneUI<UI_Inven>();
         //Dictionary<int, Data.Stat> dict = Managers.Data.StatDict;
